3c52c564279083919fe91ae4b4a0fbb410766b01
John Dulaney <john.s.dulaney1@gmail.com>
2018-04-16 20:22:59 -0500

    adding documentation

--- src/app/Main.elm
+++ src/app/Main.elm
@@ -1,9 +1,10 @@
+-- Module Declaration
 module Main exposing (..)
-
+-- Imports 
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
-
+-- Main declaration, outlines the main as including a model, a view, and an update method
 main =
     Html.beginnerProgram
         { model = model
@@ -11,24 +12,24 @@ main =
         , update = update
         }
 
--- MODEL
+-- MODEL - types each piece to the proper type. Strings galore
 type alias Model =
     { name : String
     , password : String
     , passwordAgain : String
     }
-
+-- I think lower case model is reserved somehow, anyway set it to accept what uper Case Model is, and set the string values to empty
 model : Model
 model =
     Model "" "" ""
 
--- UPDATE
+-- UPDATE - I dont get this
 type Msg
     = Name String
     | Password String
     | PasswordAgain String
 
-
+-- Update is maybe a reserved word for two way data binding?
 update : Msg -> Model -> Model
 update msg model =
     case msg of
@@ -50,7 +51,6 @@ view model =
         , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
         , viewValidation model
         ]
-
 
 viewValidation : Model -> Html msg
 viewValidation model =
