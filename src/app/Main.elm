cd2bdc012dad0f2874f9fabed4b003f20be088e6
John Dulaney <john.s.dulaney1@gmail.com>
2018-04-16 20:29:37 -0500

    :construction: Commented form in Main.js

--- src/app/Main.elm
+++ src/app/Main.elm
@@ -31,6 +31,7 @@ type Msg
 
 -- Update is maybe a reserved word for two way data binding?
 update : Msg -> Model -> Model
+-- read up on this
 update msg model =
     case msg of
         Name name ->
@@ -42,23 +43,29 @@ update msg model =
         PasswordAgain password ->
             { model | passwordAgain = password }
 
--- VIEW
+-- VIEW - View must be reserved, accepts the msg model for use on the inputs
 view : Model -> Html Msg
 view model =
+-- Construct the html elements that make the form. Apparenty you declare the type of element, then stilck the html attributes in the []
     div [id "form"]
         [ input [ type_ "text", placeholder "Name", onInput Name ] []
         , input [ type_ "password", placeholder "Password", onInput Password ] []
         , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+        -- this gets froggy right here
         , viewValidation model
         ]
-
+-- this model sets up the logic for matching the passwords and displaying the green or red html elements.
 viewValidation : Model -> Html msg
 viewValidation model =
     let
+    -- I dont get this structure
         ( color, message ) =
+        -- easy if, then, else
+        -- the () is a color, then the text. It is set belowthe logic
             if model.password == model.passwordAgain then
                 ( "green", "OK" )
             else
                 ( "red", "Passwords do not match!" )
+        -- this is where the logic above's set values are passed into the created style element. Accepts color and text.
     in
         div [ style [ ( "color", color ) ] ] [ text message ]
